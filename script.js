d9339b27dc2ea395cc3314fea50cc6ef148a5778
Martin <48533353+Brinkvisual@users.noreply.github.com>
2021-02-02 12:16:00 +0100

    pushing site

---
+++ script.js
@@ -0,0 +1,9 @@
+function meme (){
+    var image = document.getElementById("profile");
+   if (image.src =  "https://media-exp1.licdn.com/dms/image/C4E03AQH0zD0IkE8L4Q/profile-displayphoto-shrink_800_800/0/1597167475169?e=1617840000&v=beta&t=HDLx__FKJ45wNqogRuNE2y6qdZmYzmCvwT3VqvUdGWs"){
+        image.src = "https://i.chzbgr.com/full/9340630784/h357E9FA4/text-when-you-write-10-lines-of-code-without-searching-on-google-itaint-much-but-its-honest-work"
+   }
+}
+
+
+  
